5bb42d47b67c207ecc1b7a5bea1222b51e927920
freddychpo <freddychpo@gmail.com>
2019-12-25 15:45:12 -0500

    MongoDB Driver Core

---
+++ WebApiApp/Models/LivingCreature.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiApp.Models
+{
+    public class LivingCreature
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string CreatureName { get; set; }
+        public string Home { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+    }
+}
